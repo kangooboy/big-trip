4955f65c7a00d838f412bfc1b762696dfa4342a5
Kangooboy <raffan@ukr.net>
2022-05-04 00:36:47 +0300

    addEventListeners

--- src/view/filter-point-view.js
+++ src/view/filter-point-view.js
@@ -19,18 +19,21 @@ const createFilterPointTemplate = () => (
 );
 
 export default class FilterPointView {
-  getTemplate() {
-    return createFilterPointTemplate;
+  #element = null;
+
+  get template() {
+    return createFilterPointTemplate();
   }
 
-  getElement() {
-    if(!this.element) {
-      this.element = createElement(this.getTemplate());
+  get element() {
+    if(!this.#element) {
+      this.#element = createElement(this.template);
     }
-    return this.element;
+    return this.#element;
   }
 
   removeElement() {
-    this.element = null;
+    this.#element = null;
   }
 }
+
--- src/view/new-point-view.js
+++ src/view/new-point-view.js
@@ -166,18 +166,21 @@ const createNewPointTemplate = () => (
 );
 
 export default class NewPointView {
-  getTemplate() {
-    return createNewPointTemplate;
+  #element = null;
+
+  get template() {
+    return createNewPointTemplate();
   }
 
-  getElement() {
-    if(!this.element) {
-      this.element = createElement(this.getTemplate());
+  get element() {
+    if(!this.#element) {
+      this.#element = createElement(this.template);
     }
-    return this.element;
+    return this.#element;
   }
 
   removeElement() {
-    this.element = null;
+    this.#element = null;
   }
 }
+
