06af2c192ebebccbd4b242ef1d3ff6809344ed9b
Kangooboy <raffan@ukr.net>
2022-06-10 01:55:42 +0300

    fix input city name

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -90,8 +90,8 @@ const createEditPointTemplate = (data, allDestinations, allOffers) => {
             <label class="event__label  event__type-output" for="event-destination-1">
               ${type}
             </label>
-            <input class="event__input  event__input--destination" id="event-destination-1" type="text"
-            name="event-destination" ${(destination) ? `value=${destination.name}` : 'placeholder="choose target city"'} list="destination-list-1" required>
+            <input class="event__input  event__input--destination" id="event-destination-1" type="text" placeholder="choose target city"
+            name="event-destination" value="${(destination) ? destination.name : ''}" list="destination-list-1" required>
             <datalist id="destination-list-1">
               ${createDestinationList(allDestinations)}
             </datalist>
